b17b9ed3e279cf8d051b21cd6f94f1991faf6680
mattssca <mattsada@gmail.com>
2021-12-14 15:08:05 -0800

    adding function to save individual plots that are used in report format.

--- scripts/img_man.sh
+++ scripts/img_man.sh
@@ -6,6 +6,4 @@ convert -quiet out/*_plot_title.png out/*_filtered_metrics_tab.png out/*_box2.pn
 #clean folder
 rm out/*_plot_title.png
 rm out/*_box2.png
-rm out/*_discrepancies_genome_strat.png
-rm out/*_filtered_metrics_tab.png
 rm Rplots.pdf
